de7d829ca5fd0948bbb208705128f5ebb9b78e8b
Riho Okubo <rihoohkubo1002@gmail.com>
2016-04-29 16:55:06 +0800

    commit init

--- sampleAlart/ViewController.swift
+++ sampleAlart/ViewController.swift
@@ -15,6 +15,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapBtn(sender: UIButton) {
+        
+        //　アラートを作る
+        var alertController = UIAlertController(title:"タイトル", message:"メッセージ", preferredStyle: .Alert)
+        
+        //　OKボタンを追加する
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: {action in self.myOK()}))
+        
+        //TODO:キャンセルボタンを追加しよう（押されたら、キャンセルとデバッグエリアに表示される）
+        alertController.addAction(UIAlertAction(title: "cancel", style: .Cancel, handler: {action in self.myCancel() }))
+        
+        
+        
+        
+        
+        //　アラートを表示する
+        presentViewController(alertController, animated: true, completion: nil)
+        
+        
+    }
+    
+    // OKが押された時に呼ばれるメソッド
+    func myOK(){
+        print("OK")
+    }
+    
+    //　キャンセルが押された時に呼ばれるメソッド
+    func myCancel(){
+        print("キャンセル")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
